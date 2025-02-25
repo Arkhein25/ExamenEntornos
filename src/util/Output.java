b06ddcc61db3009043a355580b656a9c4ec9febe
Israel <isra025@gmail.com>
2025-02-25 20:47:31 +0100

    actualizando main y output

--- src/util/Output.java
+++ src/util/Output.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Output {
     public static void mostrarListado(List<Producto> productos) {
-        System.out.println("ID\tReferencia\tUnidades\tCreado el\tModificado el");
+        System.out.println("ID\tReferencia\tUnidaxdes\tCreado el\tModificado el");
         for (Producto producto : productos) {
             System.out.println(producto);
         }
